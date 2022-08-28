528e1612655fb5c4fd80670013f13286bc1d82ed
Yassinmoh <yassin_mohamed310@outlook.com>
2022-08-28 21:26:55 +0200

    create menu section

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
-
+import styled from 'styled-components'
 import './App.css';
+import Menu from './components/Menu'
+import Navbar from './components/Navbar'
+
+const Container = styled.div`
+  display: flex;
+`
+const Main = styled.div`
+  flex: 7;
+`
+const Wrapper = styled.div``
+
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>
+          <h1>AccountCircleIcon</h1>
+        </Wrapper>
+      </Main>
+    </Container>
   );
 }
 
---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,158 @@
+import React from 'react'
+import styled from 'styled-components'
+import yassinLogo from '../images/logo.png'
+import HomeIcon from '@mui/icons-material/Home';
+import ExploreIcon from '@mui/icons-material/Explore';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import HistoryIcon from '@mui/icons-material/History';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import MovieIcon from '@mui/icons-material/Movie';
+import ArticleIcon from '@mui/icons-material/Article';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SettingsIcon from '@mui/icons-material/Settings';
+import FlagIcon from '@mui/icons-material/Flag';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+
+
+
+
+
+const Container = styled.div`
+    flex: 1;
+    background-color:#202020;
+    height: 100vh;
+    color: white;
+    font-style: 14px;
+    position: sticky;
+    top: 0;
+`
+const Wrapper = styled.div`
+    padding: 1.125rem 1.625rem;
+`
+const Item = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1.25rem;
+    padding: 0.469rem 0;
+    cursor: pointer;
+`
+const Logo = styled.div`
+    display:flex;
+    align-items: center;
+    gap: 5px;
+    margin-bottom: 1.563rem;
+    font-weight: bold;
+`
+const Img = styled.img`
+    height: 1.563rem;
+`
+const Hr = styled.hr`
+    margin: 0.938rem 0;
+    border:  0.5px solid #373737;
+`
+const Login = styled.div`
+
+`
+const Button = styled.button`
+    padding: 5px 15px;
+    background-color: transparent;
+    border: 1px solid #3ea6ff;
+    color: #3ea6ff;
+    font-weight: 500;
+    border-radius: 3px;
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+`
+const Menu = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Logo>
+                    <Img src={yassinLogo}/>
+                    YassinTube
+                </Logo>
+                <Item>
+                    <HomeIcon/>
+                    Home
+                </Item>
+                <Item>
+                    <ExploreIcon/>
+                    Explore
+                </Item>
+                <Hr/>
+                <Item>
+                    <SubscriptionsIcon/>
+                    Subscription
+                </Item>
+                <Item>
+                    <VideoLibraryIcon/>
+                    Liprary
+                </Item>
+                <Item>
+                    <HistoryIcon/>
+                    History
+                </Item>
+                <Hr/>
+                <Login>
+                    Sign in to Like videos,Comment,and Subscripe
+                    <Button>
+                        <AccountCircleIcon/>
+                        SIGN IN
+                    </Button>
+                </Login>
+                <Hr/>
+                <Item>
+                    <LibraryMusicIcon/>
+                    Music
+                </Item>
+                <Item>
+                    <SportsBasketballIcon/>
+                    Sports
+                </Item>
+                <Item>
+                    <SportsEsportsIcon/>
+                    Gaming
+                </Item>
+                <Item>
+                    <MovieIcon/>
+                    Movies
+                </Item>
+                <Item>
+                    <ArticleIcon/>
+                    News
+                </Item>
+                <Item>
+                    <LiveTvIcon/>
+                    Live
+                </Item>
+                <Hr/>
+                <Item>
+                    <SettingsIcon/>
+                    Settings
+                </Item>
+                <Item>
+                    <FlagIcon/>
+                    Report
+                </Item>
+                <Item>
+                    <HelpOutlineIcon/>
+                    Help
+                </Item>
+                <Item>
+                    <SettingsBrightnessIcon/>
+                    Light Mode
+                </Item>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Menu
