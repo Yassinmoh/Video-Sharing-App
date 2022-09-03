cc0f7d3d69468bf0140cbc1a9ee37134d938cf2a
Yassinmoh <yassin_mohamed310@outlook.com>
2022-09-03 18:31:43 +0200

    Create video APIs

--- server/controllers/video.js
+++ server/controllers/video.js
@@ -1,4 +1,5 @@
 import Video from '../model/Video.js'
+import User from '../model/User.js'
 import { createError } from '../error.js'
 
 
@@ -28,13 +29,13 @@ export const updateVideo = async (req, res, next) => {
 
 export const deleteVideo = async (req, res, next) => {
     const video = await Video.findById(req.params.id)
-        if (!video) return next(createError(404, "Video not found"))
-        if (req.user.id === video.userId) {
-            await video.findByIdAndDelete(req.params.id)
-            res.status(200).json("the video has been deleted successfuly")
-        } else {
-            if (!video) return next(createError(403, "you onle can Delete your video"))
-        }
+    if (!video) return next(createError(404, "Video not found"))
+    if (req.user.id === video.userId) {
+        await video.findByIdAndDelete(req.params.id)
+        res.status(200).json("the video has been deleted successfuly")
+    } else {
+        if (!video) return next(createError(403, "you onle can Delete your video"))
+    }
 }
 
 export const getVideo = async (req, res, next) => {
@@ -46,3 +47,52 @@ export const getVideo = async (req, res, next) => {
         next(err)
     }
 }
+
+export const addView = async (req, res, next) => {
+    await Video.findByIdAndUpdate(req.params.id, { $inc: { views: 1 } })
+    try {
+        res.status(200).json('the view has been increased')
+    } catch (err) {
+        next(err)
+    }
+}
+
+//Get Randome Videos : aggregate
+export const random = async (req, res, next) => {
+    try {
+        const videos = await Video.aggregate([{ $sample: { size: 40 } }]);
+        res.status(200).json(videos);
+    } catch (err) {
+        next(err);
+    }
+};
+
+// Note:
+// to return trend videos set "views" => -1
+// to return last view videos set "views" => 1
+
+export const trend = async (req, res, next) => {
+    const videos = await Video.find().sort({ views: -1 })
+    try {
+        res.status(200).json(videos)
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const sub = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user.id);
+        const subscribedChannels = user.subscribedUsers;
+
+        const list = await Promise.all(
+            subscribedChannels.map(async (channelId) => {
+                return await Video.find({ userId: channelId });
+            })
+        );
+        res.status(200).json(list.flat().sort((a, b) => b.createdAt - a.createdAt))
+    } catch (err) {
+        next(err)
+    }
+}
+
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {addVideo,updateVideo,deleteVideo,getVideo } from '../controllers/video.js';
+import {addVideo,updateVideo,deleteVideo,getVideo,addView,random,trend,sub} from '../controllers/video.js';
 import {verifyToken} from '../VerifyToken.js'
 
 const routes=express.Router();
@@ -8,9 +8,9 @@ routes.post('/',verifyToken,addVideo)
 routes.put('/:id',verifyToken,updateVideo)
 routes.delete('/:id',verifyToken,deleteVideo)
 routes.get('/find/:id',getVideo)
-routes.put('/view/:id',getVideo)
-routes.get('/trend',getVideo)
-routes.get('/random',getVideo)
-routes.get('/sub',getVideo)
+routes.put('/view/:id',addView)
+routes.get('/trend',trend)
+routes.get('/random',random)
+routes.get('/sub',verifyToken,sub)
 
 export default routes
